e55bc71caf5ec6e3478fbb6a76f20bc262e6ba08
Luis Chacon <lch285@g.uky.edu>
2022-07-19 22:05:21 -0400

    this sucks

--- submitLCC.sh
+++ submitLCC.sh
@@ -2,7 +2,7 @@
 #SBATCH -t 03-00:00:00 # hours, min, sec
 #SBATCH --job-name=np_101
 #SBATCH -N nodes
-#SBATCH --partition=CAL48M192_L    #Name of the partition. See instructions in the pdf file sent to you.
+#SBATCH --partition=CAC48M192_L    #Name of the partition. See instructions in the pdf file sent to you.
 #SBATCH --account=col_sjpo228_uksr #Name of account to run under; If you are part of CTFL, don't change. If you are collaborator, email Savio Poovathingal first.
 #SBATCH --exclusive
 echo "Job running on SLURM NODELIST: $SLURM_NODELIST "
