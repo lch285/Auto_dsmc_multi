550a56ab8791ed08ccdf9d1e3999e2930bec6625
Luis Chacon <lch285@g.uky.edu>
2022-07-19 21:46:33 -0400

    this is the first commit!!

--- submitLCC.sh
+++ submitLCC.sh
@@ -6,5 +6,5 @@
 #SBATCH --account=col_sjpo228_uksr #Name of account to run under; If you are part of CTFL, don't change. If you are collaborator, email Savio Poovathingal first.
 #SBATCH --exclusive
 echo "Job running on SLURM NODELIST: $SLURM_NODELIST "
-​
+
 mpirun -np totalprocessors /project/sjpo228_uksr/LuisChacon/git/dsmc/src/spa_mpi -in dsmc.input
