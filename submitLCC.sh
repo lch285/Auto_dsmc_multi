e45710e7226946be7628648cc647202c48414f24
lch285 <109630233+lch285@users.noreply.github.com>
2022-07-19 22:31:55 -0400

    Add files via upload

--- submitLCC.sh
+++ submitLCC.sh
@@ -2,9 +2,9 @@
 #SBATCH -t 03-00:00:00 # hours, min, sec
 #SBATCH --job-name=np_101
 #SBATCH -N nodes
-#SBATCH --partition=CAC48M192_L    #Name of the partition. See instructions in the pdf file sent to you.
+#SBATCH --partition=CAL48M192_L    #Name of the partition. See instructions in the pdf file sent to you.
 #SBATCH --account=col_sjpo228_uksr #Name of account to run under; If you are part of CTFL, don't change. If you are collaborator, email Savio Poovathingal first.
 #SBATCH --exclusive
 echo "Job running on SLURM NODELIST: $SLURM_NODELIST "
-
+​
 mpirun -np totalprocessors /project/sjpo228_uksr/LuisChacon/git/dsmc/src/spa_mpi -in dsmc.input
